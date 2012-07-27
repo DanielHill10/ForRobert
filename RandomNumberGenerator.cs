585ba6ad844388743ebcb548195d6cf06c458f2e
Robert Stothard <robert@stothard.me.uk>
2012-07-27 11:07:19 +0100

    Added a Random number Generator

---
+++ RandomNumberGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace Tutorial1
+{
+    using System;
+
+    public class RandomNumberGenerator
+    {
+        private static readonly Random Rng = new Random();
+
+        public static int CalculateDamage(int attackValue)
+        {
+            return Rng.Next() * attackValue;
+        }
+    }
+}
